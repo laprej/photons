4e445605c1fa495d3147404653a7517ff5b755f2
Mark Anderson <emer@emer.net>
2011-07-15 16:48:05 -0400

    More Perl

--- refloormapsobj/OBJ/Parser.pm
+++ refloormapsobj/OBJ/Parser.pm
@@ -1,5 +1,17 @@
 #!/Users/manderso/.local/bin/perl
-package OPJ::Parser;
+
+package OBJ::Parser;
 
 use warnings;
 use strict;
+
+sub new {
+  my($class, $filename) = @_;
+  my $data = {
+	      'filename' => $filename
+	     };
+  return bless($data, $class);
+}
+
+
+1;
